1089819569f9eefbd6eed7cc2162010241813dcb
DanGabArc <134779450+DanGabArc@users.noreply.github.com>
2026-04-12 22:05:09 +0500

    Implement add method and fix remove logic in MyLinkedList
    Complete all MyList methods and add bounds checking

--- Assignment_2ADS/src/MyLinkedList.java
+++ Assignment_2ADS/src/MyLinkedList.java
@@ -27,6 +27,90 @@ public class MyLinkedList<T> implements MyList<T> {
     }
 
     @Override
+    public void add(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index, ", Size: " + size);
+        }
+
+        if (index == 0) {
+            Node<T> newNode = new Node<>(element);
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            size++;
+            return;
+        } else if (index = size) {
+            add(element);
+            return;
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        Node<T> newNode = new Node<>(element);
+        newNode.prev = current.prev;
+        newNode.next = current;
+        current.prev.next = newNode;
+        current.prev = newNode;
+        size++;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.element;
+    }
+
+    @Override
+    public T remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index, ", Size: " + size);
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        T removedElement = current.element;
+
+        if (size == 1) {
+            head = tail = null;
+        }
+
+        else if (index == 0) {
+            head = current.next;
+            head.prev = null;
+        }
+
+        else if (index == size) {
+            tail = current.prev;
+            current.next = null;
+        }
+
+        else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+
+        size--;
+        return removedElement;
+    }
+
+    @Override
     public T set(int index, T element) {
         Node<T> current = head;
         for (int i = 0; i < index; i++) {
